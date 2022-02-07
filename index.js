5f5e2a517fd240d6290f85d21f1349644921a9ce
Dora Goczi <doribea@gmail.com>
2022-02-06 19:42:18 -0500

    token server for access key generation

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const {RtcTokenBuilder, RtcRole} = require('agora-access-token');
+require('dotenv').config()
+
+
+const app = express();
+
+const nocache = (_, resp, next) => {
+    resp.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    resp.header('Expires', '-1');
+    resp.header('Pragma', 'no-cache');
+    next();
+}
+
+const generateRTCToken = (req, resp) => { 
+    resp.header('Acess-Control-Allow-Origin', '*');
+
+    const channelName = req.params.channel;
+    if (!channelName) {
+      return resp.status(500).json({ 'error': 'channel is required' });
+    }
+
+    let uid = req.params.uid;
+    if(!uid || uid === '') {
+      return resp.status(500).json({ 'error': 'uid is required' });
+    }
+    // get role
+    let role;
+    if (req.params.role === 'publisher') {
+      role = RtcRole.PUBLISHER;
+    } else if (req.params.role === 'audience') {
+      role = RtcRole.SUBSCRIBER
+    } else {
+      return resp.status(500).json({ 'error': 'role is incorrect' });
+    }
+
+    let expireTime = req.query.expiry;
+    if (!expireTime || expireTime === '') {
+      expireTime = 3600;
+    } else {
+      expireTime = parseInt(expireTime, 10);
+    }
+
+    const currentTime = Math.floor(Date.now() / 1000);
+    const privilegeExpireTime = currentTime + expireTime;
+
+    let token;
+    if (req.params.tokentype === 'userAccount') {
+      token = RtcTokenBuilder.buildTokenWithAccount(process.env.APP_ID, process.env.APP_CERTIFICATE, channelName, uid, role, privilegeExpireTime);
+    } else if (req.params.tokentype === 'uid') {
+        console.log(process.env.APP_ID);
+        console.log(process.env.APP_CERTIFICATE);
+        console.log(channelName)
+        console.log(uid);
+        console.log(role);
+        console.log(privilegeExpireTime);
+      token = RtcTokenBuilder.buildTokenWithUid(process.env.APP_ID, process.env.APP_CERTIFICATE, channelName, uid, role, privilegeExpireTime);
+    } else {
+      return resp.status(500).json({ 'error': 'token type is invalid' });
+    }
+    console.log("channel name is", channelName)
+
+    return resp.json({ 'rtcToken': token });
+};
+
+
+app.get('/rtc/:channel/:role/:tokentype/:uid', nocache , generateRTCToken)
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port: ${process.env.PORT}`);
+  });
